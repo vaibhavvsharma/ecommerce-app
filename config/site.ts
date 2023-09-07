10373eb32478799925f9a45e45a611e3f9fbe084
unknown <vaibhavvsharma@hotmail.com>
2023-09-08 03:37:58 +0530

    Initial Boilerplate Code For ecommerce-app

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Jewellery Website",
+  description: "We craft the best jewellery in india.",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
