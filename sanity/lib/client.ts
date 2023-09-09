421360d148c5edc49d09dc5507372321d84ab1db
unknown <vaibhavvsharma@hotmail.com>
2023-09-09 20:09:40 +0530

    Product Grid Updated

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,6 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token:
+   "skHHPo1GcZCODeBQnRkoGDPlFRI9KlnkEp2fT2PrDa3wbQ7a6zEv1Ldip03kgMElcujV0qz1ailQehyyGD6lJSfgPD3ZMP29Mv9iVnHteMjc1IISC6ZEOn7tqxqSp1FwBKxp9LrF9V4O02U6vr2udVHD3F0MSB2fUUsyMXihbH6yrMUbf6vN",
 })
