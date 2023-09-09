fd1017d05c6b7d145cfd74c037ed44a5bb30540f
unknown <vaibhavvsharma@hotmail.com>
2023-09-10 01:05:25 +0530

    Sorting of items: implemented

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,24 @@ import { ProductFilters } from "@/components/product-filters"
 import { ProductGrid } from "@/components/product-grid"
 import { ProductSort } from "@/components/product-sort"
 
-interface Props {}
+interface Props {
+  searchParams: {
+    date?: string
+    price?: string
+  }
+}
 
-export default async function Page() {
+export default async function Page({ searchParams }: Props) {
+  const { date = "desc", price } =  searchParams
+  const priceOrder = price
+    ? `| order(price ${price})`
+    : ""
+  const dateOrder = date
+    ? `| order(_createdAt ${date})`
+    : ""
+  const order = `${priceOrder}${dateOrder}`
   const products = await client.fetch<SanityProduct[]>(
-    groq`*[_type == "product"]{
+    groq`*[_type == "product"] ${order}{
       _id,
       _createdAt,
       name,
@@ -26,7 +39,6 @@ export default async function Page() {
       "slug": slug.current
     }`
   )
-  
 
   return (
     <div>
@@ -61,7 +73,7 @@ export default async function Page() {
             >
               <div className="hidden lg:block">{/* Product filters */}</div>
               {/* Product grid */}
-              <ProductGrid products={products} /> 
+              <ProductGrid products={products} />
             </div>
           </section>
         </main>
